69ec84d1cc1b040532868314ff9644c711e514d1
sartawi8-lang <sartawi8@gmail.com>
2026-07-06 22:43:04 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# Besiktning Pro – Hemvägen 1
+# Besiktning Pro 3.0
 
-Professionellt mobilt besiktningsprotokoll för avhysning/övertagande.
-
-## Uppdatering på GitHub
-Ladda upp dessa filer till samma repository:
-- index.html
-- manifest.json
-- sw.js
-- icon.svg
-- README.md
-
-GitHub Pages-länken fortsätter fungera:
-https://sartawi8-lang.github.io/besiktning-hemvagen1/
-
-Efter uppladdning: öppna länken och uppdatera sidan.
+Ladda upp dessa filer till GitHub Pages repository.
--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='besiktning-pro-v2';
-const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const C='bp3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
